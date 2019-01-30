34ea43bc39eab0a92582da3c30fd72041f995a53
John Nathan <john@gupdy.com>
2019-01-30 12:37:25 +0100

    add link to Telegram channel

--- web/footer.js
+++ web/footer.js
@@ -1,9 +1,13 @@
 const discordURL = "https://discord.gg/EUdryhF";
 const githubURL = "https://github.com/AscensionBlockchain/Ascension";
+const telegramURL = "https://t.me/ascensionxch";
 
 export async function Attach($body) {
   let $footer = app.render.Template("footer");
 
+  $footer.find("[data-target-telegram]").click(() => {
+    window.open(telegramURL, "_blank");
+  });
   $footer.find("[data-target-discord]").click(() => {
     window.open(discordURL, "_blank");
   });
